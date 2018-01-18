5a02fb0e65811bffdc168f211ef1e42b46b6e718
Ethan Roberts <ethan.reid.roberts@gmail.com>
2018-01-18 12:25:28 -0500

    prettified app.js for consistency

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const factoryRoutes = require('./routes/factories');
-const brandRoutes = require('./routes/brands')
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const factoryRoutes = require("./routes/factories");
+const brandRoutes = require("./routes/brands");
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/factories', factoryRoutes);
-app.use('/brands', brandRoutes);
+app.use("/factories", factoryRoutes);
+app.use("/brands", brandRoutes);
 
 // going to http://localhost:3000/ in your browser should yield 'OK'
-app.get('/', (req, res) => {
-    res.sendStatus(200);
+app.get("/", (req, res) => {
+  res.sendStatus(200);
 });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.statusCode = 404;
   next(err);
 });
 
 // error handlers in express must accept 4 parameters, so don't remove next (4th param)
 app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500);
-    res.send({
-        message: err.message,
-        error: err
-    });
+  res.status(err.statusCode || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
 });
 
 // start the web server
 const server = app.listen(3000, () => {
-    const port = server.address().port;
-    console.log('Server listening at port %s', port);
+  const port = server.address().port;
+  console.log("Server listening at port %s", port);
 });
 
 module.exports = server;
